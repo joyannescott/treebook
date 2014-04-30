d5f557e3c1009790070ba3c2c71ca7a4afb529fc
Joy Scott <joyfitz@mac.com>
2014-04-29 17:24:23 -0700

    Updated profile page, and add scoped status

--- test/integration/custom_routes_test.rb
+++ test/integration/custom_routes_test.rb
@@ -15,5 +15,9 @@ class CustomRoutesTest < ActionDispatch::IntegrationTest
    		get '/register'
    		assert_response :success
    end
+   test "that a profile page works" do 
+   		get '/dummy2'
+   		assert_response :success
+   end
 
 end
